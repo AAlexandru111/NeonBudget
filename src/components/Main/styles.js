e88177228fc71ace7a10ccbb0b8766c1f1bef7d4
AAlexandru111 <andreidotalexandru@gmail.com>
2022-05-31 14:41:13 +0300

    animations

--- src/components/Main/styles.js
+++ src/components/Main/styles.js
@@ -25,11 +25,33 @@ export default makeStyles((theme) => ({
       color: '#806914',
     }
   },
+  '@keyframes flux': {
+    "0%": {
+      textShadow: `0vw 0vw 1vw #1041FF, 0vw 0vw 3vw #1041FF, 0vw 0vw 10vw #1041FF, 0vw 0vw 10vw #1041FF, 0vw 0vw .4vw #8BFDFE, .5vw .5vw .1vw #147280`,
+      color: "#28D7FE",
+    },
+    "100%": {
+      textShadow: `0vw 0vw 1vw #1041FF, 0vw 0vw 3vw #1041FF, 0vw 0vw 10vw #1041FF, 0vw 0vw 10vw #1041FF, 0vw 0vw .4vw #8BFDFE, .5vw .5vw .1vw #147280`,
+      color: "#28D7FE",
+    },
+    "50%": {
+      textShadow: `0vw 0vw .5vw #082180, 0vw 0vw 1.5vw #082180, 0vw 0vw 5vw #082180, 0vw 0vw 5vw #082180, 0vw 0vw .2vw #082180, .5vw .5vw .1vw #0A3940`,
+      color: "#146C80",
+    }
+  },
+  '@keyframes flicker': {
+    "50%": {
+      color: 'white',
+      filter: 'saturate(200%) hue-rotate(20deg)',
+    }
+  },
   media: {
     height: 0,
     paddingTop: '56.25%', // 16:9
   },
   cardHeader: {
+    //work with this
+    // backgroundColor: 'grey',
     borderBottom: '10px solid rgba(0, 255, 0, 0.5)',
     boxShadow: "0 8px 40px -12px rgb(0 255 0 / 0.7);",
     "&:hover": {
@@ -40,11 +62,26 @@ export default makeStyles((theme) => ({
     // animation: `$animate 1.5s ease-in-out infinite alternate`,
     '& .MuiCardHeader-title': {
       fontFamily: 'Bangers',
-      fontSize: '35px',
-      textShadow: '0 0 3vw #F40A35',    
-      color: '#FB4264',
-      animation: `$neon 1s ease infinite`,
-      // lineHeight: '5vw',
+      fontSize: '3vw',
+      // textShadow: '0 0 3vw #F40A35',    
+      // color: '#FB4264',
+      // animation: `$neon 1s ease infinite`,
+      // --interval: '1s',
+      color: 'tomato',
+      textShadow: 
+        `0 0 10px var(orangered),
+        0 0 20px var(firebrick),
+        0 0 40px var(maroon),
+        0 0 80px var(darkred)`,
+      // willChange: 'filter, color',
+      filter: 'saturate(60%)',
+      animation: `$flicker steps(100) 1s 1s infinite`,
+    },
+    '& .MuiCardHeader-subheader': {
+      fontFamily: 'Bangers',
+      fontSize: '2vw',
+      // animation: `$flux 2s linear infinite`,
+      textShadow: '0vw 0vw 3vw #2356FF',
     },
   },
   expand: {
